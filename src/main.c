dcbd5b52101880c950d1fd59276ca92a11995e94
chu282 <chu282@purdue.edu>
2026-04-17 15:48:21 -0400

    whatever

--- src/main.c
+++ src/main.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include "hardware/pio.h"
 #include "hardware/dma.h"
-#include "vga.c"
+#include "vga.h"
 
 int main() {
     stdio_init_all();
 
     // initialize VGA
     init_vga();
+
+    draw_rect(100, 100, 100, 100, WHITE);
+    draw_colorboard();
 
     for (;;) {
         printf("Hello world!\n");
